027bd07eb5a2bb67860970d974a7c81f670ee41d
Brendan Davy <davybrendan@gmail.com>
2020-12-16 08:45:30 -0800

    handled POST requests

--- music_controller/api/urls.py
+++ music_controller/api/urls.py
@@ -1,7 +1,8 @@
-from .views import RoomView
+from .views import RoomView, CreateRoomView
 from django.urls import path
 
 urlpatterns = [
-    path('home', RoomView.as_view()),
+    path('room', RoomView.as_view()),
+    path('create-room', CreateRoomView.as_view()),
 
 ]
